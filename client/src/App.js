ac66a86d4b1bc7e159c219e122915e083b7e2de8
Pavel <kohnev84@gmail.com>
2023-02-22 10:31:24 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ExelAdd from './components/ExelAdd';
+import AddFlie from './components/AddFlie';
+
+function App() {
+  return (
+    <div className="App">
+      Hello <br />
+      {/* <ExelAdd /> */}
+      <AddFlie />
+    </div>
+  );
+}
+
+export default App;
